846765eec43667646fa48ff36b45479ee30f303f
Kelly Xie <kelly_xie@tcenergy.com>
2021-06-01 19:16:52 -0600

    Add navigation with routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,7 +13,11 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
-
     return heroes;
+  }
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,16 +9,11 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes:Hero[]=[];
-  selectedHero?:Hero ;
+  ;
 
   constructor(private heroService:HeroService,private messageService: MessageService) { }
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero:Hero):void{
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
   
   getHeroes():void{
